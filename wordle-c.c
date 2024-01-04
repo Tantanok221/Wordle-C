88828824df5094bff897ac83001f608966c58ba6
Tantanok221 <6572withspicy@gmail.com>
2024-01-04 14:28:02 +0800

    feat: Implemented checkGuess function

--- wordle-c.c
+++ wordle-c.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
+#include <time.h>
 #define MAX_WORDS 50
 #define MAX_LETTERS 5
 #define MAX_GUESS 5
 typedef struct Guess
 {
-  // Use MAX_WORDS to avoid potential buffer overflow(as long as user doesn't input more than 50 letters)
+  // Use MAX_WORDS to avoid potential buffer overflow(as long as user doesn't
+  // input more than 50 letters)
   char letter[MAX_WORDS];
   int result[MAX_WORDS];
   // * 0 = not guessed, 1 = correct, 2 = incorrect. 3 = misplaced
@@ -15,7 +16,7 @@ typedef struct Guess
 void printEmpty(void);
 void printGuess(char *letter, int *result);
 char *generateAnswer(void);
-int CheckGuess(char guess, char *answer);
+char* CheckGuess(char* guess, char *answer);
 char *toUpper(char *letter, int length);
 void green();
 void red();
@@ -28,7 +29,6 @@ int main(void)
   // For Debug/Init Use
   char emptyGuess[] = "     ";
   int emptyResult[] = {0, 0, 0, 0, 0};
-  
 
   // Generate answer
   char *answer = generateAnswer();
@@ -41,22 +41,23 @@ int main(void)
   }
 
   int globalIterate = 0;
+  int didGameWon = 0;
   while (globalIterate != MAX_GUESS)
   {
     char currentGuess[5];
     printEmpty();
-    printf("%s\n",answer);
     printf("----------------------wordle-clone--------------------\n");
-    printf("|Red = \033[1;31mIncorrect\033[1;0m Green = \033[1;32mCorrect\033[1;0m Yellow = \033[1;33mMisplaced\033[1;0m  |\n");
+    printf("|Red = \033[1;31mIncorrect\033[1;0m Green = "
+           "\033[1;32mCorrect\033[1;0m Yellow = \033[1;33mMisplaced\033[1;0m  "
+           "|\n");
     for (int i = 0; i < MAX_GUESS; i++)
     {
       // Debug Purpose Pls Ignore
-      int debugResult[] = {3, 1, 1, 3, 1};
-      printGuess(guesses[i].letter, debugResult);
+      // int debugResult[] = {3, 1, 1, 3, 1};
+      // printGuess(guesses[i].letter, debugResult);
       // End of Debug
 
-
-      // printGuess(guesses[i].letter, guesses[i].result);
+      printGuess(guesses[i].letter, guesses[i].result);
     }
     printf("------------------------------------------------------\n");
     printf("Guess a 5 letter word: ");
@@ -75,17 +76,27 @@ int main(void)
         capGuess[i] = currentGuess[i];
       }
     }
-    for (int i = 0; i < MAX_LETTERS; i++)
-    {
-      guesses[globalIterate].result[i] = CheckGuess(capGuess[i], answer);
-    }
-    
+
+
+    memcpy(guesses[globalIterate].result, CheckGuess(capGuess, answer), MAX_LETTERS * sizeof(int));
+
 
     // printf("%s",pushArray);
     strcpy(guesses[globalIterate].letter, capGuess);
-    // 
-
-
+    int isCorrect = 1;
+    for (int i = 0; i < MAX_LETTERS; i++)
+    {
+      if (capGuess[i] != answer[i])
+      {
+        isCorrect = 0;
+        break;
+      }
+    }
+    if (isCorrect)
+    {
+      didGameWon = 1;
+      break;
+    }
     // Handle early win and exit
 
     globalIterate++;
@@ -93,7 +104,9 @@ int main(void)
   // Print again for the fifth time (due to the way the loop is structured)
   printEmpty();
   printf("----------------------wordle-clone--------------------\n");
-  printf("|Red = \033[1;31mIncorrect\033[1;0m Green = \033[1;32mCorrect\033[1;0m Yellow = \033[1;33mMisplaced\033[1;0m  |\n");
+  printf(
+      "|Red = \033[1;31mIncorrect\033[1;0m Green = \033[1;32mCorrect\033[1;0m "
+      "Yellow = \033[1;33mMisplaced\033[1;0m  |\n");
   for (int i = 0; i < MAX_GUESS; i++)
   {
     printGuess(guesses[i].letter, guesses[i].result);
@@ -102,14 +115,58 @@ int main(void)
   // Handle win lose condition
   // If win print congratulation and exit
   // If lost print answer and exit
+  if (didGameWon)
+  {
+    printf("Congrats u won the game ( ˶ˆᗜˆ˵ )\n");
+  }
+  else
+  {
+    printf("Sorry you can't guess the letter is it too hard? ˙◠˙ the answer was: %s\n", answer);
+  }
 
   return 0;
 }
 
-int CheckGuess(char guess, char *answer)
+char* CheckGuess(char* guess, char *answer)
+// * return 0 = not guessed, 1 = correct, 2 = incorrect. 3 = misplaced
 {
-  // * return 0 = not guessed, 1 = correct, 2 = incorrect. 3 = misplaced
-  return 1;
+  int* result = malloc(sizeof(int) * MAX_LETTERS);
+  int* answerGuessed = malloc(sizeof(int) * MAX_LETTERS);
+  for(int i = 0; i < MAX_LETTERS; i++){
+    answerGuessed[i] = 0;
+  }
+  for (int i = 0; i < MAX_LETTERS; i++)
+  {
+    if (guess[i] == answer[i] && answerGuessed[i] == 0)
+    {
+      result[i] = 1;
+      answerGuessed[i] = 1;
+    }
+    else {
+      result[i] = 2;
+    }
+  }
+  for (int i = 0; i < MAX_LETTERS; i++)
+  {
+    if (guess[i] != answer[i])
+    {
+      for (int j = 0; j < MAX_LETTERS; j++)
+      {
+        if (guess[i] == answer[j] && answerGuessed[j] == 0)
+        {
+          result[i] = 3;
+          answerGuessed[j] = 1;
+          break;
+        }
+        else
+        {
+          result[i] = 2;
+        }
+      }
+    }
+  }
+
+  return result; // incorrect
 }
 
 char *generateAnswer(void)
@@ -173,7 +230,8 @@ char *generateAnswer(void)
 
 void printEmpty(void)
 {
-  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+         "\n\n\n\n\n\n\n\n\n\n\n\n");
 }
 
 void printGuess(char *letter, int *result)
@@ -182,7 +240,7 @@ void printGuess(char *letter, int *result)
   printf("|                       ");
   for (int i = 0; i < MAX_LETTERS; i++)
   {
-    
+
     if (result[i] == 1)
     { // Correct (Green)
       green();
@@ -208,18 +266,10 @@ void printGuess(char *letter, int *result)
   }
   printf("                        |\n");
 }
-void red () {
-  printf("\033[1;31m");
-}
+void red() { printf("\033[1;31m"); }
 
-void green () {
-  printf("\033[1;32m");
-}
+void green() { printf("\033[1;32m"); }
 
-void yellow () {
-  printf("\033[1;33m");
-}
+void yellow() { printf("\033[1;33m"); }
 
-void reset () {
-  printf("\033[0m");
-}
+void reset() { printf("\033[0m"); }
